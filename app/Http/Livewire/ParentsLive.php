f4397e2584f3a3bf65292a4a5e1e0dfd2ce52ae5
Baraa Ghalayini <73060154+BaraaGhalayini@users.noreply.github.com>
2023-03-10 15:06:53 +0200

    Create (Model , Table , Livewire) Parents

---
+++ app/Http/Livewire/ParentsLive.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ParentsLive extends Component
+{
+    public function render()
+    {
+        return view('livewire.parents-live');
+    }
+}
